17c3c3594cb2e5e1863355dd0fbac4b4b6ca53df
Kaan-1 <kerdogan20@ku.edu.tr>
2024-12-05 14:03:49 +0300

    main now justs runs guest without test-name input

--- test_project/methods/guest/src/main.rs
+++ test_project/methods/guest/src/main.rs
@@ -1,7 +1,1 @@
-use risc0_zkvm::{guest::env};
-
-fn main() {
-    let mut big_input: Vec<i32> = env::read();
-    let sum: i32 = big_input.iter().sum();
-    env::commit(&sum);
-}
+use risc0_zkvm :: guest :: env ; fn main () { let n : u64 = env :: read () ; ; let answer = is_prime (n) ; let answer_u32 = if answer { 1 } else { 0 } ; env :: commit (& answer_u32) ; } fn input () -> u64 { 230932049823041 } fn is_prime (n : u64) -> bool { if n <= 1 { return false ; } if n <= 3 { return true ; } if n % 2 == 0 || n % 3 == 0 { return false ; } let mut i = 5 ; while i * i <= n { if n % i == 0 || n % (i + 2) == 0 { return false ; } i += 6 ; } true }
